88098d45be57afd3bca4f9f1108f5484393d611b
Joakim Olsson <joakim.olsson@addskills.com>
2017-05-05 20:28:26 +0200

    Added a set of functions to structure the creation of Pod VHDs.

---
+++ Create-ArchivesVHD.ps1
@@ -0,0 +1,32 @@
+﻿. "$PSScriptRoot\Create-PodVHD.ps1"
+
+function Create-ArchivesVHD {
+    param(
+        $PackageName,
+        $Label=$PackageName,
+        $DestinationDir="C:\",
+        $MaxSize=128GB,
+        [Switch]$Deduplicated,
+        [Switch]$Fixed
+    )
+
+    $params = @{
+        PodPath="$DestinationDir$PackageName.archives.vhdx"
+        Label=$Label
+        DestinationDir=$DestinationDir
+        MAxSize=$MaxSize
+    }
+
+    if ( !($PSBoundParameters.ContainsKey("Deduplicated")) ) {
+        $params.NonDeduplicated = $true
+    }
+    if ($PSBoundParameters.ContainsKey("Fixed")) {
+        $params.Fixed = $true
+    }
+
+    $vhd = Create-PodVHD @params
+    
+    $path = $vhd | Get-Disk | Get-Partition | Get-Volume | Find-VolumePath
+    
+    return $vhd
+}
---
+++ Create-PodVHD.ps1
@@ -0,0 +1,105 @@
+﻿. "$PSScriptRoot\Find-VolumePath.ps1"
+
+<#
+
+.SYNOPSIS
+Creates a VHD used as a pod for the Caffeine delivery system.
+
+.DESCRIPTION
+This is a basic function used by the Create-HiveVHD and Create-ArchivesVHD functions.
+
+#>
+function Create-PodVHD {
+    param(
+        $PodPath,
+        $Label,
+        $DestinationDir="C:\",
+        $MaxSize=128GB,
+        [Switch]$NonDeduplicated,
+        [Switch]$Fixed
+    )
+
+    if ($DestinationDir -notmatch "[\\/]$") {
+        $DestinationDir += "\"
+    }
+
+    if ( !(Test-Path $DestinationDir) ) {
+        mkdir $DestinationDir | Out-Null
+    }
+
+    $params = @{
+        Path=$PodPath
+        SizeBytes=$MaxSize
+    }
+
+    if ($Fixed) {
+        $params.Fixed = $true
+    } else {
+        $params.Dynamic = $true
+    }
+
+    Write-Host "Creating VHD... " -NoNewline
+    $vhd = New-VHD @params
+    Write-Host "Done!" -ForegroundColor Green
+
+    Write-Host "Formatting VHD... " -NoNewline
+    $vhd = $vhd | Mount-VHD -Passthru
+
+    $disk = $vhd | Get-Disk
+
+    $disk | Initialize-Disk -PartitionStyle MBR
+
+    $partition = $disk | New-Partition -UseMaximumSize -MbrType IFS
+
+    $volume = $partition | Format-Volume -FileSystem NTFS -NewFileSystemLabel $Label -Force -Confirm:$false
+    Write-Host "Done!" -ForegroundColor Green
+
+    Write-Host "Assigning driveletter... " -NoNewline
+    $dlcs = "DEFGHIJKLMNOPQRSTUVWXYZ"
+    Get-Volume | ? { $_.DriveLetter } | % {
+        $dlcs = $dlcs -replace $_.DriveLetter,""
+    }
+    $driveLetter = $dlcs[0]
+
+    $id =   if ($volume.UniqueID) {
+                $volume.UniqueID # >= Win10/WS2016
+            } else {
+                $volume.ObjectID # <= Win8.1/WS2012R2
+            }
+
+    $idRegex = $id -replace "\\","\\"
+    $w32_volume = gwmi "Win32_Volume where DeviceID='$idRegex'"
+
+    $w32_volume.DriveLetter = "$driveLetter`:"
+    $w32_volume.Put() | Out-Null
+    Write-Host "Done! ($driveLetter)" -ForegroundColor Green
+
+    if (!$NonDeduplicated) {
+        Write-Host "Setting deduplication on the VHD volume... " -NoNewline
+        if ( (Get-Module -Name Deduplication -ea SilentlyContinue -ListAvailable) ) {
+            Import-Module Deduplication
+            $volume | Enable-DedupVolume -UsageType HyperV | Out-Null
+            Write-Host "Done!" -ForegroundColor Green
+        } else {
+            Write-Host "Deduplication feature not installed!" -ForegroundColor Red
+        }
+    }
+    
+    $path = Find-VolumePath $volume
+
+    try {
+        Write-Host "Setting access rights for the volume... " -NoNewline
+        $acl = Get-Acl $Path
+        $ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "FullControl", "Allow")
+        $acl.SetAccessRule($ar)
+        Set-Acl $path $acl
+        Write-Host "Done!" -ForegroundColor Green
+    } catch {
+        Write-Host "Failed!" -ForegroundColor Red
+        Write-Host $_
+    }
+
+
+
+    return $vhd
+}
