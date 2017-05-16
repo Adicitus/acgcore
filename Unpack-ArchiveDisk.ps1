4aa0b7e361a690cbe4403f93cadf7aff9f9dd84c
Joakim Olsson <joakim.olsson@addskills.com>
2017-05-16 12:23:46 +0200

    Rewrote 'Unpack-ARchiveDisk' to use mountpoints instead of looking at driveletters. Also added in a fallback for deployments on WS2012, where mounting and unmounting VHDs may cause a situation where mountpoints become unavailable within the current context (even though it is accessible from other contexts). By generating a new mountpoint for the VHD, the VHD's contents can be accessed.

--- Unpack-ArchiveDisk.ps1
+++ Unpack-ArchiveDisk.ps1
@@ -1,11 +1,14 @@
 ﻿. "$PSScriptRoot\ShoutOut.ps1"
 . "$PSScriptRoot\Run-Operation.ps1"
+. "$PSScriptRoot\Find-VolumePath.ps1"
 . "$PSScriptRoot\Unpack-RARFile.ps1"
 
 
 # Unpacks a MOCSetup-style Archive disk (Cluster Pod).
-function Unpack-ArchiveDisk($File){
+function Unpack-ArchiveDisk {
     
+    param([parameter(ValueFromPipeline=$true)][System.IO.FileInfo]$File)
+
     shoutOut "Unpacking '$($File.FullName)'..." Cyan
     Mount-DiskImage -ImagePath $file.fullname
     $vhd = Get-DiskImage -ImagePath $File.FullName
@@ -13,12 +16,20 @@ function Unpack-ArchiveDisk($File){
     
     $partitions | % {
         shoutOut "looking at partition #$($_.PartitionNumber)... " Cyan
-        if ($_.DriveLetter -notmatch "^[A-Z]$") {
-            shoutOut "No drive letter associated with the partition, skipping!"
+        $sfxPath = $_ | Get-Volume | Find-VolumePath
+        if (!$sfxPath) {
+            shoutOut "No Mount Point found for this partition, skipping!"
             return
         }
 
-	    $SfxPath = "$($_.driveletter):\"
+        if (!($sfxPath = $sfxPath | ? { Test-Path $_ } | Select -First 1)) {
+            Write-Host "For some reason the assigned access paths have become invalid, generate a new one..." -ForegroundColor Yellow
+            $tmpDir = "C:\temp\{0:x}" -f [datetime]::Now.Ticks
+            mkdir $tmpDir | Out-Null
+            $_ | Add-PartitionAccessPath -AccessPath $tmpDir
+            $sfxPath = $tmpDir
+            Write-Host "Added '$tmpDir'!" -ForegroundColor Green
+        }
     
 	    $SfxFiles = Get-ChildItem -Path $SfxPath -Filter *.exe
     
@@ -26,6 +37,13 @@ function Unpack-ArchiveDisk($File){
 	    {
             Unpack-RARFile $sfx
 	    }
+        
+        if ($tmpDir) {
+            Write-Host "Cleaning up temporary access path..." -ForegroundColor Cyan -NoNewline
+            $_ | Remove-PartitionAccessPath -AccessPath $tmpDir
+            Remove-Item $tmpDir -Recurse -Force
+            Write-Host "Done!" -ForegroundColor Green
+        }
     }
 	$vhd | Dismount-DiskImage
 }
