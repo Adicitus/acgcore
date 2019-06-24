bd4e776480d0e57dec2d612e0146dba4f1be1770
Joakim Olsson <joakim.olsson@addskills.com>
2019-06-24 11:32:21 +0200

    Updated Write-ConfigFile to match Parse-ConfigFile more closely.

--- Write-ConfigFile.ps1
+++ Write-ConfigFile.ps1
@@ -5,27 +5,46 @@ function Write-ConfigFile {
     )
 
 	[string[]]$output = @()
-	foreach ($key in $Config.keys)
+	$keys = $Config.keys
+
+	$keys = $Keys | Sort-Object
+
+	foreach ($key in $keys)
 	{
-		$output += "[$key]" #`r`n"
+		$output += "[$key]"
 		foreach($item in $config[$key].keys)
 		{
             foreach ($value in $config[$key][$item])
             {
-			    $output += (($item,"$value".trimend() | Where-Object {$_}) -join '=') # + "`r`n"
+				if (!$value) {
+					# Entry, just append it to the output
+					$output += $item
+					continue
+				}
+
+				# Setting, Append <item>=<value> to output for each value.
+				$value = $value.Replace("#", "\#").trimend() 
+			    $output += (
+					($item, $value | Where-Object {$_}) -join '='
+				)
             }
 		}
+		$output += "" # Empty line between each section to make output more readable.
 	}
+
+
 	if ($PSBoundParameters.ContainsKey('Path'))
 	{
 		if (!(test-path $Path))
 		{
 			new-item -itemtype file -force -Path $Path | out-null
 		}
-		$output | out-file -force -filepath $Path 
+		[System.IO.File]::WriteAllLines($Path, $output, [System.Text.Encoding]::UTF8)
+		#$output | out-file -force -filepath $Path 
 	}
 	else
 	{
 		$output
 	}
+
 }
