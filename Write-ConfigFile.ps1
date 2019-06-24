4d8abe4ac3b2ff03855b4baf523af897f4833332
Joakim Olsson <joakim.olsson@addskills.com>
2019-06-24 11:34:38 +0200

    Updated Write-ConfigFile.ps1 to match project format guidelines.

--- Write-ConfigFile.ps1
+++ Write-ConfigFile.ps1
@@ -4,47 +4,41 @@ function Write-ConfigFile {
         [string]$Path
     )
 
-	[string[]]$output = @()
-	$keys = $Config.keys
+    [string[]]$output = @()
+    $keys = $Config.keys
 
-	$keys = $Keys | Sort-Object
+    $keys = $Keys | Sort-Object
 
-	foreach ($key in $keys)
-	{
-		$output += "[$key]"
-		foreach($item in $config[$key].keys)
-		{
-            foreach ($value in $config[$key][$item])
-            {
-				if (!$value) {
-					# Entry, just append it to the output
-					$output += $item
-					continue
-				}
+    foreach ($key in $keys) {
+        $output += "[$key]"
+        foreach ($item in $config[$key].keys) {
+            foreach ($value in $config[$key][$item]) {
+                if (!$value) {
+                    # Entry, just append it to the output
+                    $output += $item
+                    continue
+                }
 
-				# Setting, Append <item>=<value> to output for each value.
-				$value = $value.Replace("#", "\#").trimend() 
-			    $output += (
-					($item, $value | Where-Object {$_}) -join '='
-				)
+                # Setting, Append <item>=<value> to output for each value.
+                $value = $value.Replace("#", "\#").trimend() 
+                $output += (
+                    ($item, $value | Where-Object { $_ }) -join '='
+                )
             }
-		}
-		$output += "" # Empty line between each section to make output more readable.
-	}
+        }
+        $output += "" # Empty line between each section to make output more readable.
+    }
 
 
-	if ($PSBoundParameters.ContainsKey('Path'))
-	{
-		if (!(test-path $Path))
-		{
-			new-item -itemtype file -force -Path $Path | out-null
-		}
-		[System.IO.File]::WriteAllLines($Path, $output, [System.Text.Encoding]::UTF8)
-		#$output | out-file -force -filepath $Path 
-	}
-	else
-	{
-		$output
-	}
+    if ($PSBoundParameters.ContainsKey('Path')) {
+        if (!(test-path $Path)) {
+            new-item -itemtype file -force -Path $Path | out-null
+        }
+        [System.IO.File]::WriteAllLines($Path, $output, [System.Text.Encoding]::UTF8)
+        #$output | out-file -force -filepath $Path 
+    }
+    else {
+        $output
+    }
 
 }
