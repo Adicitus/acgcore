1febc583acb0c936fc18c77b8595410e3b74d791
Joakim Olsson <joakim.olsson@addskills.com>
2019-06-27 17:11:29 +0200

    Fixed a bug where the ACGCore file regex would not allow '.' in file names.

--- RegexPatterns.ps1
+++ RegexPatterns.ps1
@@ -7,7 +7,7 @@ $IPv4NMB = $Script:RegexPatterns.IPv4NetMaskByte
 
 $ItemChars = "[^\\/:*`"|<>]"
 $Script:RegexPatterns.Directory = '(?<directory>(?<root>[A-Z]+:|\.|\\.*)[\\/]({0}+[\\/]?)*)' -f $ItemChars
-$Script:RegexPatterns.File = ( '(?<file>(?<directory>((?<root>[A-Z]+:|\.|\\.*)[\\/])?({0}+[\\/])*)(?<filename>([^\\/.]+)+(\.(?<extension>[^\\/.]+)?))' + ")" ) -f $ItemChars
+$Script:RegexPatterns.File = ( '(?<file>(?<directory>((?<root>[A-Z]+:|\.|\\.*)[\\/])?({0}+[\\/])*)(?<filename>([^\\/]+)+(\.(?<extension>[^\\/.]+)?))' + ")" ) -f $ItemChars
 $Script:RegexPatterns.IPv4Address = "($IPv4AB\.){3}$($IPv4AB)"
 $Script:RegexPatterns.IPv4Netmask = "((255\.){3}$IPv4NMB)|((255\.){2}($IPv4NMB\.)0)|((255\.){1}($IPv4NMB\.)0\.0)|(($IPv4NMB\.)0\.0\.0)|0\.0\.0\.0"
 $Script:RegexPatterns.GUID = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{10}"
