c5f87a1c205671ca714225d9922a790f764ee861
Joakim Olsson <joakim.olsson@addskills.com>
2020-02-20 23:32:46 +0100

    Added a Cmdlet to set windows Admin Autologon.

---
+++ source/os/Set-WinAutoLogon.ps1
@@ -0,0 +1,53 @@
+#Set-WinAutoLogon.ps1
+
+function Set-WinAutoLogon {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, Position=1, ParameterSetName="Credential")]
+        [pscredential]$LogonCredential,
+        [Parameter(Mandatory=$true, Position=1, ParameterSetName="Params")]
+        [String]$Username,
+        [Parameter(Mandatory=$true, Position=2, ParameterSetName="Params")]
+        [SecureString]$Password,
+        [Parameter(Mandatory=$false, Position=3, ParameterSetName="Params")]
+        [String]$Domain=".",
+        [Parameter(Mandatory=$false)]
+        [int]$AutoLogonLimit=100000
+    )
+
+    $templatePath = "$PSScripRoot\.assets\templates\winlogon.tmplt.reg"
+    $Values = $null
+
+    switch ($PSCmdlet.ParameterSetName) {
+
+        "Params" {
+            $values = @{
+                Username    = $Username
+                Password    = Unlock-SecureString $Password
+                Domain      = $Domain
+            }
+        }
+
+        "Credential" {
+            $values = @{
+                Domain      = "."
+                Password    = Unlock-SecureString $LogonCredential.Password
+            }
+            $LogonCredential.UserName -match "((?<domain>.+)\\)?(?<username>.+)"
+            if ($matches.domain) {
+                $v.domain = $matches.domain
+            }
+            $v.Username = $matches.Username
+        }
+
+    }
+    
+    $valeus.AutoLogonLimit = $AutoLogonLimit
+
+    $tmpFile = [System.IO.Path]::GetTempFileName()
+
+    Rendter-Template $templatePath $values > $tmpFile
+
+    reg import $tmpFile
+    Remove-Item $tmpFile
+}
