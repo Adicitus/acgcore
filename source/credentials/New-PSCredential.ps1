b3ff8fd1af5aab1d9ffa125a22dc4a34ce3b7d8c
Joakim Olsson <joakim.olsson@addskills.com>
2020-03-30 10:59:59 +0200

    Added support for SecureString to New-PSCredential

--- source/credentials/New-PSCredential.ps1
+++ source/credentials/New-PSCredential.ps1
@@ -6,12 +6,18 @@
     Takes a Username and Password to create a PSCredential.
 #>
 function New-PSCredential{
+    [CmdletBinding(DefaultParameterSetName="ClearText")]
     param(
         [parameter(Mandatory=$true, position=1)][string] $Username,
-        [parameter(Mandatory=$true, position=2)][string] $Password
+        [parameter(Mandatory=$true, position=2, ParameterSetName="ClearText")][string] $Password,
+        [parameter(Mandatory=$true, position=2, ParameterSetName="SecureString")][securestring]$SecurePassword
     )
-    $secPassw  = ConvertTo-SecureString $password -AsPlainText -Force
-    $cred = New-Object System.Management.Automation.PSCredential($username, $secPassw)
+
+    if ($PSCmdlet.ParameterSetName -eq "ClearText") {
+        $SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+    }
+
+    $cred = New-Object System.Management.Automation.PSCredential($username, $SecurePassword)
 
     return $cred
 }
